e889b33459733bf104076d3af1b97246990a8932
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-04-10 16:33:06 -0300

    Create a rock-paper-scissors game.

---
+++ rock-paper-scissors/main.py
@@ -0,0 +1,72 @@
+from random import choice
+
+options = ('rock', 'paper', 'scissors')
+conditions = {
+    'paper': {
+        'win': 'rock',
+        'lose': 'scissors',
+    },
+    'rock': {
+        'win': 'scissors',
+        'lose': 'paper',
+    },
+    'scissors': {
+        'win': 'paper',
+        'lose': 'rock',
+    }
+}
+images_options = {
+    'paper': '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+''',
+    'rock':  '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+''',
+    'scissors': '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+''',
+}
+
+
+def print_result_game(first: str, second: str) -> str:
+    first_option = f"User choose: \n{images_options.get(first, '')}\n"
+    second_option = f"Computer choose: \n{images_options.get(second, '')}\n"
+    return first_option + second_option
+
+
+def choose_winner(user: str, computer: str) -> str:
+    result = "Draw"
+    condition = conditions.get(user, None)
+    if not condition:
+        return "Invalid option"
+    if condition['win'] == computer:
+        result = "You Win"
+    if condition['lose'] == computer:
+        result = "You lose"
+    return result
+
+
+def select_game_option(user: str) -> str:
+    computer_option = choice(options)
+    game_winner = choose_winner(user, computer_option)
+    return print_result_game(user, computer_option) + game_winner
+
+
+if __name__ == "__main__":
+    user_option = input("What do you choose? Type rock, paper or scissors\n")
+    print(select_game_option(user_option))
