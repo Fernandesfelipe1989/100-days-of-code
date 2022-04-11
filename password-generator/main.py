8bc2a9bc01d2f89f667f100cce0626a3c75311ab
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-04-11 13:51:56 -0300

    Create a password generator.

--- password-generator/main.py
+++ password-generator/main.py
@@ -1,13 +1,24 @@
+import string
 from random import choices, shuffle
+
+all_letter_choices = string.ascii_letters
+all_symbols_choices = "@&_"
+all_digits_choices = string.digits
 
 
 def password_generator(letters: int, symbols: int, numbers: int) -> str:
-    return ""
+    letters_password = choices(all_letter_choices, k=letters)
+    digits_password = choices(all_digits_choices, k=numbers)
+    symbols_password = choices(all_symbols_choices, k=symbols)
+    password = letters_password + digits_password + symbols_password
+    print(password)
+    shuffle(password)
+    return "".join(password)
 
 
 if __name__ == "__main__":
     print("Welcome to the PyPassword Generator")
     size_letters = int(input('How many letters would you like in your password:\n'))
     size_symbols = int(input('How many symbols would you like in your password:\n'))
-    size_numbers = int(input('How manu numbers would you like in your password:\n'))
+    size_numbers = int(input('How many numbers would you like in your password:\n'))
     print(f"Here is your password: {password_generator(size_letters, size_symbols, size_numbers)}")
