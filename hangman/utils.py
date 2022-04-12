9e9a35ce3f619ed879e3a77c33274e54ddd81ca4
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-04-12 14:32:00 -0300

    Remove the \n in the choose word.

--- hangman/utils.py
+++ hangman/utils.py
@@ -1,14 +1,16 @@
 from random import choice
 
-INPUT = 'palavras.txt'
+INPUT = 'words.txt'
 words = []
+
+# TODO: Necessary remove special character
 
 
 def initialize_hangman():
     with open(INPUT, 'r') as input_document:
         for word in input_document:
             words.append(word)
-    return choice(words)
+    return choice(words).replace("\n", "")
 
 
 if __name__ == "__main__":
