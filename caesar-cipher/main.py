643eb0887f68fe70df0f4aae9bca04ddc22525a5
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-04-14 18:29:01 -0300

    Create the decode flow.

--- caesar-cipher/main.py
+++ caesar-cipher/main.py
@@ -16,6 +16,24 @@ def encode_shit_method(letter: str, shift: int) -> str:
     return LETTERS[letter_index]
 
 
+def caesar_decryption(message: str, shift) -> str:
+    decode_message = (decode_shit_method(letter, shift)for letter in message.lower())
+    return "".join(decode_message)
+
+
+def decode_shit_method(letter: str, shift: int) -> str:
+    if not letter in LETTERS:
+        return letter
+
+    letter_index = LETTERS.index(letter)
+    letter_index -= shift
+
+    if letter_index < 0:
+        letter_index -= 1
+
+    return LETTERS[letter_index]
+
+
 def caesar_encryption(message: str, shift: int) -> str:
     encode_message = (encode_shit_method(letter, shift) for letter in message.lower())
     return "".join(encode_message)
@@ -24,4 +42,7 @@ def caesar_encryption(message: str, shift: int) -> str:
 if __name__ == "__main__":
     user_message = input("Type your message:\n")
     shift_number = int(input("Type the shift number:\n"))
+    encode_message = caesar_encryption(user_message, shift_number)
     print(caesar_encryption(user_message, shift_number))
+    decode = caesar_decryption(encode_message, shift_number)
+    print(decode)
