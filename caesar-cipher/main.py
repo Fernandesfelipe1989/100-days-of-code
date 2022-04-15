eeb5317fbad4f6ba67b5cf799e6e7aed48f99a39
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-04-15 20:53:28 -0300

    Correct the bug of the shift bigger than the max size.

--- caesar-cipher/main.py
+++ caesar-cipher/main.py
@@ -3,22 +3,28 @@ from string import ascii_lowercase
 from utils import LOGO
 
 LETTERS = [letter for letter in ascii_lowercase]
+MAX_SIZE = len(LETTERS)
 
 
 def shit_method(letter: str, shift: int, action: str) -> str:
-    if letter in LETTERS:
-        letter_index = LETTERS.index(letter)
-        letter_index += shift if action == 'encode' else shift * -1
+    is_letter = letter in LETTERS
 
-        if letter_index > len(LETTERS):
-            letter_index = (letter_index % len(LETTERS)) - 1
+    if not is_letter:
+        return letter
 
-        if letter_index < 0:
-            letter_index -= 1
+    if shift > MAX_SIZE:
+        shift = shift % MAX_SIZE
 
-        return LETTERS[letter_index]
+    letter_index = LETTERS.index(letter)
+    letter_index += shift if action == 'encode' else shift * -1
 
-    return letter
+    if letter_index > MAX_SIZE - 1:
+        letter_index = letter_index - MAX_SIZE
+
+    if letter_index < 0:
+        letter_index -= 1
+
+    return LETTERS[letter_index]
 
 
 def caesar_cipher_process(message: str, shift: int, action: str) -> str:
@@ -28,13 +34,16 @@ def caesar_cipher_process(message: str, shift: int, action: str) -> str:
 
 if __name__ == "__main__":
     print(LOGO)
-    direction = input("Type encode to encrypt, or decode to decrypt:\n").lower()
-    user_message = input("Type your message:\n").lower()
-    shift_number = int(input("Type the shift number:\n"))
+    stop_condition = "y"
+    while stop_condition == "y":
+        direction = input("Type encode to encrypt, or decode to decrypt:\n").lower()
+        user_message = input("Type your message:\n").lower()
+        shift_number = int(input("Type the shift number:\n"))
 
-    if direction == 'encode':
-        print(f"The encoded text is {caesar_cipher_process(user_message, shift_number, 'encode')}")
-    elif direction == 'decode':
-        print(f"The decoded text is {caesar_cipher_process(user_message, shift_number, 'decode')}")
-    else:
-        print('Invalid option. So bye-bye')
+        if direction == 'encode':
+            print(f"The encoded text is {caesar_cipher_process(user_message, shift_number, 'encode')}")
+        elif direction == 'decode':
+            print(f"The decoded text is {caesar_cipher_process(user_message, shift_number, 'decode')}")
+        else:
+            print('Invalid option. So bye-bye')
+        stop_condition = input("Type y if you want to go again, Otherwise type n\n").lower()
