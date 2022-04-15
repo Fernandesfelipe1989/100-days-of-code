00d23769936444d70420c1ac9bbe0ccb1f174486
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-04-15 20:13:30 -0300

    WIP

--- caesar-cipher/main.py
+++ caesar-cipher/main.py
@@ -1,5 +1,7 @@
 from string import ascii_lowercase
 
+from utils import LOGO
+
 LETTERS = [letter for letter in ascii_lowercase]
 
 
@@ -8,8 +10,8 @@ def shit_method(letter: str, shift: int, action: str) -> str:
         letter_index = LETTERS.index(letter)
         letter_index += shift if action == 'encode' else shift * -1
 
-        if letter_index > len(LETTERS) - 1:
-            letter_index = letter_index - len(LETTERS) + 1
+        if letter_index > len(LETTERS):
+            letter_index = (letter_index % len(LETTERS)) - 1
 
         if letter_index < 0:
             letter_index -= 1
@@ -18,13 +20,14 @@ def shit_method(letter: str, shift: int, action: str) -> str:
 
     return letter
 
-   
+
 def caesar_cipher_process(message: str, shift: int, action: str) -> str:
     result_message = (shit_method(letter, shift, action) for letter in message.lower())
     return "".join(result_message)
 
 
 if __name__ == "__main__":
+    print(LOGO)
     direction = input("Type encode to encrypt, or decode to decrypt:\n").lower()
     user_message = input("Type your message:\n").lower()
     shift_number = int(input("Type the shift number:\n"))
