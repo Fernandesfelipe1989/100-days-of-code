c942d399c9c259b28e3ddbdb6e5c4610217e23b8
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-04-16 15:48:35 -0300

    WIP

--- calculator/main.py
+++ calculator/main.py
@@ -18,9 +18,13 @@ INVALID_MESSAGE = "Invalid operation"
 
 if __name__ == "__main__":
     print(LOGO)
+    run_process = "yes"
     first_number = int(input("What's the first number?\n"))
-    select_operation = input("Pick an operation: \n +, -, *, or /:\n")
-    second_number = int(input("What's the second number?\n"))
-    math_operation = MATH_OPERATIONS.get(select_operation, None)
-    output_format = "{} {} {} = ".format(first_number, select_operation, second_number)
-    print(f'{output_format}{math_operation(first_number, second_number)}' if select_operation else INVALID_MESSAGE)
+    while run_process == 'yes':
+        select_operation = input("Pick an operation: \n +, -, *, or /:\n")
+        second_number = int(input("What's the second number?\n"))
+        math_operation = MATH_OPERATIONS.get(select_operation, None)
+        output_format = "{} {} {} = ".format(first_number, select_operation, second_number)
+        print(f'{output_format}{math_operation(first_number, second_number)}' if select_operation else INVALID_MESSAGE)
+        run_process = input(f"Type yes to continue calculating {first_number}, or type n to start a new calculation:\n")
+
