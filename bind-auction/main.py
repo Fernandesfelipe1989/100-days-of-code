ea9883588d247c653d3bcda3cd27fd7f0ee4a64b
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-04-15 22:09:52 -0300

    Put the clear function in the final step in the while loop.

--- bind-auction/main.py
+++ bind-auction/main.py
@@ -7,12 +7,13 @@ if __name__ == "__main__":
     print("Welcome to the secret auction program")
     auction = {}
     run_auction = 'yes'
+    
     while run_auction == 'yes':
         name = input("What's your name:\n")
-        clear()
         bid = int(input("What's your bid?: $"))
         auction[name] = bid
         run_auction = input("Are there any other bidders? Type yes or no\n").lower()
+        clear()
     winner = max(auction, key=lambda x: auction[x])
     print(f"The winner is {winner} a bid of ${auction[winner]}")
 
