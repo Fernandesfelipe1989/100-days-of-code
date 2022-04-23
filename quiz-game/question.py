8043863090587a4d77bec503a5d6700114f2b44e
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-04-23 18:11:16 -0300

    Fixed the init bug in the question class.

--- quiz-game/question.py
+++ quiz-game/question.py
@@ -1,14 +1,22 @@
+from data import QUESTIONS_DATA
+
 
 class Question:
 
-    def __int__(self, text: str, answer: str) -> None:
+    def __init__(self, text: str, answer: str) -> None:
         self.text = text
-        self.answer = answer
+        self.answer = bool(answer)
 
     def __repr__(self):
         return self.text
 
-    def correct_answer(self, answer: bool) -> bool:
-        if answer == self.answer:
+    def correct_answer(self, answer: str) -> bool:
+        if bool(answer) == self.answer:
             return True
         return False
+
+
+QUESTION_BANK = [Question(text=question['text'], answer=question['answer']) for question in QUESTIONS_DATA]
+
+if __name__ == "__main__":
+    print(QUESTION_BANK)
