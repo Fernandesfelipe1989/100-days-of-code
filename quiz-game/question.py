52dec2b05854d9f5587071861c2d52c9dd91ec2d
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-04-23 18:20:30 -0300

    Fixed the bug in the answer and create basic test.

--- quiz-game/question.py
+++ quiz-game/question.py
@@ -5,18 +5,19 @@ class Question:
 
     def __init__(self, text: str, answer: str) -> None:
         self.text = text
-        self.answer = bool(answer)
+        self.answer = answer
 
     def __repr__(self):
         return self.text
 
     def correct_answer(self, answer: str) -> bool:
-        if bool(answer) == self.answer:
-            return True
-        return False
+        return answer == self.answer
 
 
 QUESTION_BANK = [Question(text=question['text'], answer=question['answer']) for question in QUESTIONS_DATA]
 
 if __name__ == "__main__":
-    print(QUESTION_BANK)
+    assert QUESTION_BANK[0].text == QUESTIONS_DATA[0]['text']
+    assert QUESTION_BANK[0].answer == QUESTIONS_DATA[0]['answer']
+    assert QUESTION_BANK[0].correct_answer(QUESTIONS_DATA[0]['answer'])
+    assert not QUESTION_BANK[0].correct_answer('False' if QUESTIONS_DATA[0]['answer'] == 'True' else 'True')
