864a05a8994294b6d27aa3483693a3f50a13ce0d
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-04-24 18:52:42 -0300

    Create a spirograph.

--- day-18.py
+++ day-18.py
@@ -57,36 +57,50 @@ class Jimmy(Turtle):
             self.right(angle)
             self.forward(size)
 
+    @staticmethod
+    def create_rgb_colour() -> tuple[int]:
+        return tuple(random.randint(0, 255) for _ in range(0, 3))
+
     def create_shapes(self):
         for shape in DRAW_SHAPE:
-            self.color(tuple(random.randint(0, 255) for _ in range(0, 3)))
+            self.color(self.create_rgb_colour())
             self.draw_shape(number_sides=shape.number_sides, size=shape.size, angle=shape.angle)
 
     def create_random_walk(self):
         self.speed(0)
         self.hideturtle()
-        self.shapesize()
         self.width(5)
         move_options = [self.forward, self.backward]
         turn_options = [self.right, self.left]
 
         for _ in range(0, 1000):
-            self.color(tuple(random.randint(0, 255) for _ in range(0, 3)))
+            self.color(self.create_rgb_colour())
             move_action = choice(move_options)
             turn_action = choice(turn_options)
             move_action(20)
             turn_action(90)
 
+    def create_spirograph(self, size_gap):
+        self.speed(0)
+        self.shape('arrow')
+        # self.hideturtle()
+        self.width(1)
+        for current in range(int(360/size_gap)):
+            self.color(self.create_rgb_colour())
+            self.circle(100)
+            self.setheading(self.heading() + size_gap)
+
 
 if __name__ == "__main__":
     timmy = Jimmy()
     colormode(255)
-    timmy.initialize('black', 'arrow')
-    timmy.draw_dash_line(50, 10)
-    timmy.reset()
-    timmy.create_shapes()
-    timmy.reset()
-    timmy.create_random_walk()
-    timmy.reset()
+    # timmy.initialize('black', 'arrow')
+    # timmy.draw_dash_line(50, 10)
+    # timmy.reset()
+    # timmy.create_shapes()
+    # timmy.reset()
+    # timmy.create_random_walk()
+    # timmy.reset()
+    timmy.create_spirograph(5)
     screen = Screen()
     screen.exitonclick()
