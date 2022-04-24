5c4644a6b5eea32ff71f644c7341bc448785190a
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-04-24 18:12:16 -0300

    Create a rando walk function.

--- day-18.py
+++ day-18.py
@@ -1,3 +1,4 @@
+import random
 from random import choice
 from turtle import Turtle, Screen
 
@@ -63,8 +64,25 @@ class Jimmy(Turtle):
     def create_shapes(self):
         for shape in DRAW_SHAPE:
             color = choice(COLOR)
-            timmy.color(color)
+            self.color(color)
             self.draw_shape(number_sides=shape.number_sides, size=shape.size, angle=shape.angle)
+
+    def create_random_walk(self):
+        self.speed(0)
+        # self.shape('circle')
+        self.hideturtle()
+        self.shapesize()
+        self.width(5)
+        move_options = [self.forward, self.backward]
+        turn_options = [self.right, self.left]
+
+        for _ in range(0, 1000):
+            color = choice(COLOR)
+            self.color(color)
+            move_action = choice(move_options)
+            turn_action = choice(turn_options)
+            move_action(20)
+            turn_action(90)
 
 
 if __name__ == "__main__":
@@ -73,5 +91,8 @@ if __name__ == "__main__":
     timmy.draw_dash_line(50, 10)
     timmy.reset()
     timmy.create_shapes()
+    timmy.reset()
+    timmy.create_random_walk()
+    timmy.reset()
     screen = Screen()
     screen.exitonclick()
