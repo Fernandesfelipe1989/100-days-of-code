83164d10392ad3e010aaaae813bfb14721f1cb64
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-04-24 20:48:30 -0300

    WIP

--- hirst-painting/main.py
+++ hirst-painting/main.py
@@ -1,1 +1,42 @@
+from random import choice
+from turtle import colormode, Screen, Turtle
+
 from utils import COLOURS
+
+
+def create_columns(obj, direction):
+    move_direction = obj.forward if direction else obj.backward
+    for _ in range(10):
+        obj.color(choice(COLOURS))
+        obj.stamp()
+        move_direction(25)
+
+
+def turn_right(obj):
+    obj.right(90)
+    obj.forward(25)
+    obj.left(90)
+
+
+def turn_left(obj):
+    obj.left(90)
+    obj.forward(25)
+    obj.right(90)
+
+
+if __name__ == "__main__":
+    jimmy = Turtle()
+    colormode(255)
+    jimmy.shape('circle')
+    jimmy.penup()
+    direction = False
+    for line in range(10):
+        create_columns(jimmy, direction)
+        if direction:
+            turn_right(jimmy)
+            direction = False
+        else:
+            turn_left(jimmy)
+            direction = True
+    screen = Screen()
+    screen.exitonclick()
