a143f79f76c7922edcdaa9ce1cfe8d25d8c805b5
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-04-23 21:14:37 -0300

    Request the question from the opentdb API.

--- quiz-game/data.py
+++ quiz-game/data.py
@@ -1,14 +1,19 @@
-QUESTIONS_DATA = [
-{"text": "A slug's blood is green.", "answer": "True"},
-{"text": "The loudest animal is the African Elephant.", "answer": "False"},
-{"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-{"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-{"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
-{"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
-{"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-{"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-{"text": "Google was originally called 'Backrub'.", "answer": "True"},
-{"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-{"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-{"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
-]
+from re import sub
+import requests
+
+PATTERN = r'[^a-zA-Z_ ]'
+URL_BASE = "https://opentdb.com/api.php?amount=25&type=boolean"
+
+
+def text_filter(text):
+    return sub(PATTERN, "", text, count=100)
+
+
+response = requests.get(URL_BASE).json()
+data = [data for data in response['results']]
+QUESTIONS_DATA = [{'text': text_filter(information['question']),
+                   'answer': information['correct_answer']} for information in data]
+
+
+if __name__ == "__main__":
+    print(QUESTIONS_DATA[0])
