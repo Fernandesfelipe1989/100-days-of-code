14128fa7f6de26fa86c17e13325645bec26f79e0
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-04-25 10:26:58 -0300

    WIP

--- day-19.py
+++ day-19.py
@@ -1,36 +1,54 @@
 from turtle import Turtle, Screen
+from random import randint
+COLOURS = ('red', 'orange', 'yellow', 'green', 'blue', 'purple')
 
-timmy = Turtle()
+
+def initialize(height, border):
+    turtles = []
+    step = (height - border) // len(COLOURS)
+    next_y_pos = -(height // 2) + border
+    for color in COLOURS:
+        turtle = Turtle()
+        turtle.shape('turtle')
+        turtle.color(color)
+        turtle.penup()
+        turtle.goto(x=-240, y=next_y_pos)
+        next_y_pos += 60
+        turtles.append(turtle)
+    return turtles
+
+
+def check_winner(x_finish_pos, racers):
+    finish = False
+    winner = None
+    for racer in racers:
+        x = racer.xcor()
+        print(x)
+        if x >= x_finish_pos:
+            finish = True
+            winner = racer.color()
+            break
+    return finish, winner
+
+
+def race(racers):
+    finish = False
+    while not finish:
+        for _ in range(10):
+            move_racers(racers=racers)
+        finish, winner = check_winner(x_finish_pos=0, racers=racers)
+    print(winner)
+
+
+def move_racers(racers):
+    for racer in racers:
+        racer.forward(randint(0, 10))
+
+
 screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Who's the winner?", prompt="Which turtle will win the race?").lower()
+racers = initialize(height=400, border=20)
+race(racers=racers)
 
-
-def move_forwards():
-    timmy.forward(10)
-
-
-def move_backwards():
-    timmy.backward(10)
-
-
-def move_counter_clockwise():
-    timmy.left(10)
-
-
-def move_clockwise():
-    timmy.right(10)
-
-
-def clear():
-    timmy.clear()
-    timmy.penup()
-    timmy.home()
-    timmy.pendown()
-
-
-screen.listen()
-screen.onkey(key='w', fun=move_forwards)
-screen.onkey(key='s', fun=move_backwards)
-screen.onkey(key='a', fun=move_counter_clockwise)
-screen.onkey(key='d', fun=move_clockwise)
-screen.onkey(key='c', fun=clear)
 screen.exitonclick()
