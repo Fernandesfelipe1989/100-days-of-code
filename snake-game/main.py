9950121e49e00e1b2eee258f2a0f074242e1e76a
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-04-26 15:37:50 -0300

    Create a snake class.

--- snake-game/main.py
+++ snake-game/main.py
@@ -1,17 +1,23 @@
 from time import sleep
 from turtle import Screen, Turtle
 
-INITIAL_SIZE = 3
 
-START_POSITION = ((0, 0), (-20, 0), (-40, 0))
+class Snake:
+    START_POSITION = ((0, 0), (-20, 0), (-40, 0))
 
+    def __init__(self):
+        self.segments = [self.create_snake_part(position) for position in self.START_POSITION]
 
-def create_snake_part(position):
-    instance = Turtle('square')
-    instance.color('white')
-    instance.penup()
-    instance.goto(position)
-    return instance
+    @staticmethod
+    def create_snake_part(pos):
+        instance = Turtle('square')
+        instance.color('white')
+        instance.penup()
+        instance.goto(pos)
+        return instance
+
+    def __len__(self):
+        return len(self.segments)
 
 
 if __name__ == "__main__":
@@ -21,13 +27,13 @@ if __name__ == "__main__":
     screen.bgcolor('black')
     screen.title("My Snake Game")
     screen.tracer(0)
-    segments = [create_snake_part(position=position) for position in START_POSITION]
+    snake = Snake()
 
     while game_is_on:
         screen.update()
         sleep(0.1)
-        for seg_num in range(len(segments) - 1, 0, -1):
-            position = segments[seg_num - 1].position()
-            segments[seg_num].goto(position)
-        segments[0].forward(20)
+        for seg_num in range(len(snake) - 1, 0, -1):
+            position = snake.segments[seg_num - 1].position()
+            snake.segments[seg_num].goto(position)
+        snake.segments[0].forward(20)
     screen.exitonclick()
