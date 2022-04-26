888638e675669b15168a859114fc4e2f21c0f3d6
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-04-26 15:25:05 -0300

    Create the forward function.

--- snake-game/main.py
+++ snake-game/main.py
@@ -1,23 +1,33 @@
+from time import sleep
 from turtle import Screen, Turtle
 
 INITIAL_SIZE = 3
 
+START_POSITION = ((0, 0), (-20, 0), (-40, 0))
 
-def move_forward():
-    print("Here")
-    snake.forward(20)
+
+def create_snake_part(position):
+    instance = Turtle('square')
+    instance.color('white')
+    instance.penup()
+    instance.goto(position)
+    return instance
 
 
 if __name__ == "__main__":
+    game_is_on = True
     screen = Screen()
     screen.setup(width=600, height=600)
     screen.bgcolor('black')
     screen.title("My Snake Game")
-    snake = Turtle()
-    snake.penup()
-    snake.shape('square')
-    snake.color('white')
-    snake.turtlesize(stretch_len=INITIAL_SIZE, stretch_wid=1)
+    screen.tracer(0)
+    segments = [create_snake_part(position=position) for position in START_POSITION]
 
-
+    while game_is_on:
+        screen.update()
+        sleep(0.1)
+        for seg_num in range(len(segments) - 1, 0, -1):
+            position = segments[seg_num - 1].position()
+            segments[seg_num].goto(position)
+        segments[0].forward(20)
     screen.exitonclick()
