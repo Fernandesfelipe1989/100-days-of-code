2ea15201ac481bb0869d214895bc477f401a9ff8
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-04-29 20:29:44 -0300

    Create the car_manage flow.

--- turtle-crossing/car_manager.py
+++ turtle-crossing/car_manager.py
@@ -7,23 +7,52 @@ STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    def __init__(self):
-        self.move_distance = STARTING_MOVE_DISTANCE
-        self.cars = [Car() for _ in range(randint(5, 20))]
-
-    def move_cars(self):
-        for car in self.cars:
-            car.forward(self.move_distance)
-
-
 class Car(Turtle):
+
+    X_MAX_POS = 280
+    Y_MAX_POS = 250
+
     def __init__(self):
         super().__init__()
         self.shape('square')
         self.shapesize(stretch_wid=1, stretch_len=2)
         self.color(choice(COLORS))
         self.penup()
-        self.goto(randint(-250, 250), randint(-250, 250))
+        self.goto((randint(-self.X_MAX_POS, self.X_MAX_POS), randint(-self.Y_MAX_POS, self.Y_MAX_POS)))
         self.setheading(180)
+
+    def has_gone(self):
+        if self.xcor() > -self.X_MAX_POS:
+            return False
+        return True
+
+    def reset_position(self):
+        self.color(choice(COLORS))
+        self.goto((self.X_MAX_POS, randint(-self.Y_MAX_POS, self.Y_MAX_POS)))
+
+
+class CarManager:
+
+    def __init__(self):
+        self.move_distance = STARTING_MOVE_DISTANCE
+        self.cars = [Car() for _ in range(randint(10, 20))]
+
+    def move_cars(self):
+        for car in self.cars:
+            car.forward(self.move_distance)
+
+    def reset_position_cars_manage(self):
+        for car in self.cars:
+            if car.has_gone():
+                car.reset_position()
+
+    def check_collision(self, instance):
+        for car in self.cars:
+            if car.distance(instance) < 20:
+                self.move_distance = 0
+                return True
+        return False
+
+    def increment_move_distance(self):
+        self.move_distance += MOVE_INCREMENT
 
--- turtle-crossing/main.py
+++ turtle-crossing/main.py
@@ -6,29 +6,34 @@ from player import Player
 from scoreboard import Scoreboard
 
 if __name__ == "__main__":
-
+    # Screen Config
     screen = Screen()
     screen.setup(width=600, height=600)
     screen.tracer(0)
+
+    # Create the instance of the game
     player = Player()
     scoreboard = Scoreboard()
     car_manager = CarManager()
 
+    # Control Config
     screen.listen()
     screen.onkey(fun=player.up, key="Up")
 
-    # TODO: Remove this part
-    screen.onkey(fun=player._test_has_collision, key="Down")
-
     game_is_on = True
-    collision = False
-    while game_is_on:
-        car_manager.move_cars()
 
+    while game_is_on:
+        # Control the cars flow
+        car_manager.move_cars()
+        car_manager.reset_position_cars_manage()
+
+        # Check if the player win the level
         if player.finish_level():
             scoreboard.increment_level()
-        # Update this part
-        if player.collision:
+            car_manager.increment_move_distance()
+
+        # Check if was a collision
+        if car_manager.check_collision(player):
             scoreboard.reset_level()
 
         time.sleep(0.1)
