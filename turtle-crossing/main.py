cf56de5cab26d1cfe1f5231b46b90bb1b4cd84c6
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-04-29 14:06:37 -0300

    Create the flow of the turtle move.

--- turtle-crossing/main.py
+++ turtle-crossing/main.py
@@ -6,11 +6,17 @@ from player import Player
 from scoreboard import Scoreboard
 
 if __name__ == "__main__":
+
     screen = Screen()
     screen.setup(width=600, height=600)
     screen.tracer(0)
+    player = Player()
+
+    screen.listen()
+    screen.onkey(fun=player.up, key="Up")
 
     game_is_on = True
     while game_is_on:
+        player.finish_level()
         time.sleep(0.1)
         screen.update()
--- turtle-crossing/player.py
+++ turtle-crossing/player.py
@@ -13,6 +13,15 @@ class Player(Turtle):
         self.shapesize(stretch_wid=1, stretch_len=1)
         self.color('black')
         self.penup()
+        self.initialize()
 
     def up(self):
         self.forward(MOVE_DISTANCE)
+
+    def finish_level(self):
+        return self.ycor() > FINISH_LINE_Y and self.initialize()
+
+    def initialize(self):
+        self.setheading(90)
+        self.goto(STARTING_POSITION)
+
