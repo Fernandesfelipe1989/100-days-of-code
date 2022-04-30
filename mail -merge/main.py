cb6703f50a247fbd677be39035fd352c64732874
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-04-30 19:14:22 -0300

    Create the class to create the personalize letters message.

--- mail -merge/main.py
+++ mail -merge/main.py
@@ -6,3 +6,27 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+class Mail:
+
+    def __init__(self):
+        self.invited_names = []
+        with open("./Input/Letters/starting_letter.txt", 'r') as message:
+            self.letter = message.read()
+        with open("./Input/Names/invited_names.txt", 'r') as invited:
+            for name in invited:
+                self.invited_names.append(name.replace("\n", ""))
+
+    def create_personalize_letter_message(self):
+        for name in self.invited_names:
+            message = self.letter.replace("[name]", name)
+            with open(f"./Output/ReadyToSend/letter_for_{name}.txt", 'a') as letter:
+                letter.write(message)
+
+    def __repr__(self):
+        return self.letter
+
+
+if __name__ == "__main__":
+    mail = Mail()
+    mail.create_personalize_letter_message()
