2efb8f04887bab9f8229b229ff02839f95d6ab70
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-04-30 15:35:13 -0300

    Update the behavior of the play again flow.

--- turtle-crossing/main.py
+++ turtle-crossing/main.py
@@ -9,34 +9,38 @@ if __name__ == "__main__":
     # Screen Config
     screen = Screen()
     screen.setup(width=600, height=600)
-    screen.tracer(0)
-
+    play_again = 'yes'
     # Create the instance of the game
-    player = Player()
-    scoreboard = Scoreboard()
-    car_manager = CarManager()
+    while play_again == "yes":
+        screen.tracer(0)
+        player = Player()
+        scoreboard = Scoreboard()
+        car_manager = CarManager()
 
-    # Control Config
-    screen.listen()
-    screen.onkey(fun=player.up, key="Up")
+        # Control Config
+        screen.listen()
+        screen.onkey(fun=player.up, key="Up")
 
-    game_is_on = True
+        game_is_on = True
 
-    while game_is_on:
-        # Control the cars flow
-        car_manager.move_cars()
-        car_manager.reset_position_cars_manage()
+        while game_is_on:
+            # Control the cars flow
+            car_manager.move_cars()
+            car_manager.reset_position_cars_manage()
 
-        # Check if the player win the level
-        if player.finish_level():
-            scoreboard.increment_level()
-            car_manager.increment_move_distance()
+            # Check if the player win the level
+            if player.finish_level():
+                scoreboard.increment_level()
+                car_manager.increment_move_distance()
 
-        # Check if was a collision
-        if car_manager.check_collision(player):
-            scoreboard.reset_level()
-            # game_is_on = False
-            screen.onkey(fun=None, key="Up")
+            # Check if was a collision
+            if car_manager.check_collision(player):
+                scoreboard.reset_level()
+                game_is_on = False
+                screen.onkey(fun=None, key="Up")
 
-        time.sleep(0.1)
-        screen.update()
+            time.sleep(0.1)
+            screen.update()
+        play_again = screen.textinput(prompt='Do you want play again? Yes or No', title="Game over")
+        play_again = play_again and play_again.lower()
+        screen.clearscreen()
