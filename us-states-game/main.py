86dcf6c6df7dc221cb9970d37dfbdde9db7bd5ce
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-05-02 10:03:10 -0300

    Create the flow to write the state name in the coor.

--- us-states-game/main.py
+++ us-states-game/main.py
@@ -1,5 +1,7 @@
 import turtle
 
+import pandas
+
 IMAGE = "blank_states_img.gif"
 
 FONT = ("Courier", 12, "normal")
@@ -12,15 +14,20 @@ class State(turtle.Turtle):
         self.color('black')
         self.penup()
         self.hideturtle()
-        self.goto(x, y)
+        self.goto(int(x), int(y))
         self.write(name, align='center', font=FONT)
 
-        
+
 if __name__ == "__main__":
+    states = pandas.read_csv("50_states.csv")
+    print(states)
     screen = turtle.Screen()
     screen.title("U.S. States Game")
     screen.addshape(IMAGE)
     turtle.shape(IMAGE)
     answer_state = screen.textinput(prompt="What's another state's name?", title="Guess the State")
-    answer_state = answer_state and answer_state.lower()
+    answer_state = answer_state and answer_state.title()
+    data_state = states[states['state'] == answer_state]
+
+    not data_state.empty and State(name=answer_state, x=data_state.x, y=data_state.y)
     turtle.mainloop()
