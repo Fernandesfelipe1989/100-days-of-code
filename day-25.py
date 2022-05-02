deb2bc6914d8fc417dd0a803e205c43d7b3a92b0
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-05-02 09:09:47 -0300

    Data analysis using pandas.

--- day-25.py
+++ day-25.py
@@ -3,24 +3,15 @@
 import pandas
 
 if __name__ == "__main__":
-    data = pandas.read_csv("weather_data.csv")
-    # print(data)
-    # data_dict = data.to_dict()
-    # print(data_dict)
-    temperature_column = data['temp'].to_list()
-    # print(temperature_column)
-    # avg = sum(temperature_column) / len(temperature_column)
-    # print(f"{avg:.1f}")
-    # print(data['temp'].max())
-    # print(data[data.day == "Monday"])
-    # print(data[data.temp == data.temp.max()])
-    # monday = data[data.day == "Monday"]
-    # monday_temp = int(monday.temp)
-    # monday_temp_f = monday_temp * 9/5 + 32
-    # print(monday_temp_f)
-    data_dict = {
-        "students": ["Amy", "James", "Angela"],
-        "scores": [76, 56, 65]
+    data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+    info_squirrel = {
+        'Fur Color': ['grey', 'red', 'black'],
+        'Count': []
     }
-    data_frame = pandas.DataFrame(data_dict)
-    data_frame.to_csv("new_data.csv")
+    gray = data[data["Primary Fur Color"] == "Gray"]
+    black = data[data["Primary Fur Color"] == "Black"]
+    red = data[data["Primary Fur Color"] == "Cinnamon"]
+    info_squirrel['Count'] = [gray.X.count(), red.X.count(), black.X.count()]
+    data_squirrel = pandas.DataFrame(info_squirrel)
+    data_squirrel.to_csv('squirrel_count.csv')
+
