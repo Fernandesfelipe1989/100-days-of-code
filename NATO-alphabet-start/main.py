b58ba5afc4f358b8669c3afce80f136958ff3d3a
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-05-02 14:58:40 -0300

    Create the Nato flow.

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -1,27 +1,11 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
-
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
-
 import pandas
-student_data_frame = pandas.DataFrame(student_dict)
 
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
+if __name__ == "__main__":
+    nato_data = pandas.read_csv('nato_phonetic_alphabet.csv')
+    nato_dictionary = {row.letter: row.code for _, row in nato_data.iterrows()}
+    word = input("Word? \n")
+    word = word and word.upper()
+    result = [nato_dictionary.get(letter) for letter in word if nato_dictionary.get(letter)]
+    print(result)
 
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
-
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
