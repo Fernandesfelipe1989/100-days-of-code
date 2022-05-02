450b1a9a2fc08a1b98100c117aed87c78415088d
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-05-02 15:04:38 -0300

    Update the message,

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -2,9 +2,9 @@ import pandas
 
 if __name__ == "__main__":
     nato_data = pandas.read_csv('nato_phonetic_alphabet.csv')
-    nato_dictionary = {row.letter: row.code for _, row in nato_data.iterrows()}
-    word = input("Word? \n")
-    word = word and word.upper()
+    nato_dictionary = {row.letter.lower(): row.code for _, row in nato_data.iterrows()}
+    word = input("Enter a wThoord? \n")
+    word = word and word.lower()
     result = [nato_dictionary.get(letter) for letter in word if nato_dictionary.get(letter)]
     print(result)
 
