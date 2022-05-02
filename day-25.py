bd473c4530bb4c87f1a64e31015f3f687c3d1e55
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-05-02 08:36:12 -0300

    WIP

--- day-25.py
+++ day-25.py
@@ -5,4 +5,10 @@ import pandas
 if __name__ == "__main__":
     data = pandas.read_csv("weather_data.csv")
     print(data)
-
+    data_dict = data.to_dict()
+    print(data_dict)
+    temperature_column = data['temp'].to_list()
+    print(temperature_column)
+    avg = sum(temperature_column) / len(temperature_column)
+    print(f"{avg:.1f}")
+    print(data['temp'].max())
