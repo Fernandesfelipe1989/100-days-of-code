8df0fda76f85e8f127f5485796b6b2b804357a33
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-05-03 22:07:21 -0300

    Studying the pack, place, and grid mode.

--- day-27.py
+++ day-27.py
@@ -1,23 +1,26 @@
 import tkinter
 
-window = tkinter.Tk()
-window.title("My first Gui program")
-window.minsize(width=500, height=300)
-
-label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
-label.pack()
-
 
 def button_clicked():
     message =_input.get()
     label.config(text=message)
 
 
+window = tkinter.Tk()
+window.title("My first Gui program")
+window.minsize(width=500, height=300)
+
+label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
+label.grid(column=0, row=0)
+
 button = tkinter.Button(text='Click Me', command=button_clicked)
-button.pack()
+button.grid(column=1, row=1)
+
+button_2 = tkinter.Button(text="New button")
+button_2.grid(column=2, row=0)
 
 _input = tkinter.Entry(width=10)
-_input.pack()
+_input.grid(column=3, row=2)
 
 
 window.mainloop()
