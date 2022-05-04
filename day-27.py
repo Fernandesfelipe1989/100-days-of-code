79148913980388ab39043ab0d58037dc0e8f286d
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-05-03 21:40:53 -0300

    Studying about buttons in tkinter.

--- day-27.py
+++ day-27.py
@@ -5,5 +5,19 @@ window.title("My first Gui program")
 window.minsize(width=500, height=300)
 
 label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
-label.pack(side="left")
+label.pack()
+
+
+def button_clicked():
+    message =_input.get()
+    label.config(text=message)
+
+
+button = tkinter.Button(text='Click Me', command=button_clicked)
+button.pack()
+
+_input = tkinter.Entry(width=10)
+_input.pack()
+
+
 window.mainloop()
