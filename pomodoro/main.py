32b6ba7089bcd5f7c39e07f228e8885f4f190604
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-05-05 08:44:50 -0300

    Initial commit.

---
+++ pomodoro/main.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = tk.Tk()
+window.title("Pomodoro")
+
+window.mainloop()
