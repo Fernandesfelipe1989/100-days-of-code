bc7828b9595580ed73a14a0714ea845a10ef13a1
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-05-05 09:12:50 -0300

    Create the widgets to the pomodoro.

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -18,5 +18,22 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = tk.Tk()
 window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW,)
+window.resizable(False, False)
+
+# Create the widgets
+timer_text = tk.Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 35, "bold"))
+timer_text.grid(column=1, row=0)
+start_button = tk.Button(text="Start", fg='black', bg=YELLOW)
+start_button.grid(column=0, row=2)
+reset_button = tk.Button(text="Reset", fg='black', bg=YELLOW)
+reset_button.grid(column=2, row=2)
+check_mark = tk.Label(text='✓', fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
+check_mark.grid(column=1, row=3)
+tomato_image = tk.PhotoImage(file="tomato.png")
+canvas = tk.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+canvas.create_image(100, 112, image=tomato_image)
+canvas.create_text(100, 140, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
 
 window.mainloop()
