d4efd051307b9e3dbd7aa461c8b01e8793ed0660
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-05-08 19:16:13 -0300

    Create the change word flow.

--- flash-card/main.py
+++ flash-card/main.py
@@ -13,15 +13,25 @@ MOTHER_LANGUAGE = "English"
 
 def selected_word(data: pd.DataFrame) -> tuple:
     row = randint(0, len(data) - 1)
-    selected_data = data.iloc[row]
-    return selected_data
+    word_info = data.iloc[row].to_dict()
+    language_1, language_2 = word_info.keys()
+    word, word_translation = word_info.values()
+    return (language_1, language_2), (word, word_translation)
+
+
+def update_canvas_text():
+    languages, word = selected_word(data)
+    canvas.itemconfig(language_text, text=languages[0])
+    canvas.itemconfig(word_text, text=word[0])
 
 
 def known_word_flow():
+    update_canvas_text()
     pass
 
 
 def unknown_word_flow():
+    update_canvas_text()
     pass
 
 
@@ -31,7 +41,7 @@ if __name__ == "__main__":
     except FileNotFoundError:
         raise FileNotFoundError("The data file was not find.")
     else:
-        study_language, mother_language = selected_word(data)
+        languages, word = selected_word(data)
 
     # ------------------- UI Designer -------------------
     window = tk.Tk()
@@ -46,17 +56,17 @@ if __name__ == "__main__":
     canvas.grid(row=0, column=0, columnspan=2)
 
     # Card Front Language text
-    language_text = canvas.create_text(400, 150, text=STUDY_LANGUAGE, font=(FONT, 40, "italic"))
-    word_text = canvas.create_text(400, 263, text=study_language, font=(FONT, 60, "bold"))
+    language_text = canvas.create_text(400, 150, text=languages[0], font=(FONT, 40, "italic"))
+    word_text = canvas.create_text(400, 263, text=word[0], font=(FONT, 60, "bold"))
 
     # Wrong button
     image_wrong = tk.PhotoImage(file="./images/wrong.png")
-    unknown_button = tk.Button(image=image_wrong, highlightthickness=0, border=0)
+    unknown_button = tk.Button(image=image_wrong, highlightthickness=0, border=0, command=unknown_word_flow)
     unknown_button.grid(row=1, column=0)
 
     # Right button
     image_right = tk.PhotoImage(file="./images/right.png")
-    know_button = tk.Button(image=image_right, highlightthickness=0, border=0)
+    know_button = tk.Button(image=image_right, highlightthickness=0, border=0, command=known_word_flow)
     know_button.grid(row=1, column=1)
 
     window.mainloop()
