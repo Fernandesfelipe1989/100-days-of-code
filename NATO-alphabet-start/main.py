0e6669a4f2ae8953d9bf6db46d0232174dff1904
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-05-07 21:48:45 -0300

    Studying exceptions.

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -3,9 +3,15 @@ import pandas
 if __name__ == "__main__":
     nato_data = pandas.read_csv('nato_phonetic_alphabet.csv')
     nato_dictionary = {row.letter.lower(): row.code for _, row in nato_data.iterrows()}
-    word = input("Enter a wThoord? \n")
-    word = word and word.lower()
-    result = [nato_dictionary.get(letter) for letter in word if nato_dictionary.get(letter)]
-    print(result)
+    is_over = False
+    while not is_over:
+        word = input("Enter a word? \n")
+        try:
+            result = [nato_dictionary[letter] for letter in word.lower()]
+        except KeyError:
+            print("Sorry, only letters in the alphabet please")
+        else:
+            print(result)
+            is_over = True
 
 
