bf1709687f4850464a2c504219e11d8a963a6803
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-05-08 18:23:55 -0300

    Insert the text in the front card.

--- flash-card/main.py
+++ flash-card/main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 
 BACKGROUND_COLOR = "#B1DDC6"
-
+FONT = 'Ariel'
 
 # ------------------- UI Designer -------------------
 window = tk.Tk()
@@ -19,6 +19,10 @@ image_card_front = tk.PhotoImage(file="./images/card_front.png")
 card_front = tk.Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front.create_image(800//2, 526//2, image=image_card_front)
 card_front.grid(row=0, column=0, columnspan=2)
+
+# Card Front Language text
+front_language_text = card_front.create_text(400, 150, text="French", font=(FONT, 40, "italic"))
+front_word_text = card_front.create_text(400, 263, text="trouve", font=(FONT, 60, "bold"))
 
 # Wrong button
 image_wrong = tk.PhotoImage(file="./images/wrong.png")
