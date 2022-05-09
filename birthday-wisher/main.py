e1dfaea9706b4e91c57dd979342b26767a8fae5f
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-05-09 20:21:10 -0300

    Fixed the bug to send email.

--- birthday-wisher/main.py
+++ birthday-wisher/main.py
@@ -4,7 +4,6 @@ import smtplib
 
 from decouple import config
 
-
 DAY_WEEK = {
     0: 'Monday',
     1: "Tuesday",
@@ -14,29 +13,29 @@ DAY_WEEK = {
     5: 'Saturday',
     6: 'Sunday',
 }
-if __name__ == "__main__":
 
-    with open('quotes.txt', 'r') as file:
-        messages = [line for line in file]
 
-    host = config('EMAIL_HOST', default="smtp.mailtrap.io")
-    port = config('EMAIL_PORT', default="2525")
-    user = config('EMAIL_HOST_USER', default="")
-    password = config('EMAIL_HOST_PASSWORD', default="")
+with open('quotes.txt', 'r') as file:
+    messages = [line for line in file]
 
-    sender = config("EMAIl_SENDER", default="teste@gmail.com")
-    receiver = config("EMAIl_RECEIVER", default="teste@gmail.com")
+host = config('EMAIL_HOST', default="smtp.mailtrap.io")
+port = config('EMAIL_PORT', default="2525")
+user = config('EMAIL_HOST_USER', default="")
+password = config('EMAIL_HOST_PASSWORD', default="")
 
-    message = f"""\
-    Subject: Motivational quote
-    To: {receiver}
-    From: {sender}
-    
-    This is a test e-mail message."""
-    current_day = dt.datetime.now()
-    day_of_week = current_day.weekday()
-    if DAY_WEEK.get(day_of_week).lower() == "monday":
-        with smtplib.SMTP(host=host, port=port) as server:
-            server.login(user=user, password=password)
-            print('Send Email')
-            server.sendmail(sender, receiver, message)
+sender = config("EMAIl_SENDER", default="teste@gmail.com")
+receiver = config("EMAIl_RECEIVER", default="teste@gmail.com")
+
+message = f"""\
+Subject: Motivational quote
+To: {receiver}
+From: {sender}
+
+{choice(messages)}"""
+current_day = dt.datetime.now()
+day_of_week = current_day.weekday()
+if DAY_WEEK.get(day_of_week).lower() == "monday":
+    with smtplib.SMTP(host=host, port=port) as server:
+        server.login(user=user, password=password)
+        print('Send Email')
+        server.sendmail(sender, receiver, message)
