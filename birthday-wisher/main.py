a636e751c752ed708b58dd738eb94de343834f38
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-05-09 20:13:26 -0300

    Create a send email flow  with  motivational quotes.

--- birthday-wisher/main.py
+++ birthday-wisher/main.py
@@ -1,33 +1,42 @@
-# import smtplib
-# from decouple import config
-#
-#
-# host = config('EMAIL_HOST', default="smtp.mailtrap.io")
-# port = config('EMAIL_PORT', default="2525")
-# user = config('EMAIL_HOST_USER', default="")
-# password = config('EMAIL_HOST_PASSWORD', default="")
-#
-#
-# sender = config("EMAIl_SENDER", default="teste@gmail.com")
-# receiver = config("EMAIl_RECEIVER", default="teste@gmail.com")
-# receiver = "A Test User <to@example.com>"
-#
-# message = f"""\
-# Subject: Hi Mailtrap
-# To: {receiver}
-# From: {sender}
-#
-# This is a test e-mail message."""
-# connection = smtplib.SMTP(host=host, port=port)
-# connection.starttls()
-# connection.login(user=user, password=password)
-# connection.sendmail(from_addr=sender, to_addrs=receiver, msg=message)
-# connection.close()
-
 import datetime as dt
+from random import choice
+import smtplib
 
-my_birthday = dt.datetime(year=2015, month=3, day=9)
-print(my_birthday)
-now = dt.datetime.now()
-print(now.year)
-print(now.day)
+from decouple import config
+
+
+DAY_WEEK = {
+    0: 'Monday',
+    1: "Tuesday",
+    2: "Wednesday",
+    3: 'Thursday',
+    4: 'Friday',
+    5: 'Saturday',
+    6: 'Sunday',
+}
+if __name__ == "__main__":
+
+    with open('quotes.txt', 'r') as file:
+        messages = [line for line in file]
+
+    host = config('EMAIL_HOST', default="smtp.mailtrap.io")
+    port = config('EMAIL_PORT', default="2525")
+    user = config('EMAIL_HOST_USER', default="")
+    password = config('EMAIL_HOST_PASSWORD', default="")
+
+    sender = config("EMAIl_SENDER", default="teste@gmail.com")
+    receiver = config("EMAIl_RECEIVER", default="teste@gmail.com")
+
+    message = f"""\
+    Subject: Motivational quote
+    To: {receiver}
+    From: {sender}
+    
+    This is a test e-mail message."""
+    current_day = dt.datetime.now()
+    day_of_week = current_day.weekday()
+    if DAY_WEEK.get(day_of_week).lower() == "monday":
+        with smtplib.SMTP(host=host, port=port) as server:
+            server.login(user=user, password=password)
+            print('Send Email')
+            server.sendmail(sender, receiver, message)
