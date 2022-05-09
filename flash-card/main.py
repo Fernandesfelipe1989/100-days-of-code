2003a312e90ec9e7f58f5289416478a3f6eaeb4d
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-05-09 10:21:43 -0300

    Create the export word to learn flow.

--- flash-card/main.py
+++ flash-card/main.py
@@ -5,7 +5,8 @@ import pandas as pd
 
 
 BACKGROUND_COLOR = "#B1DDC6"
-DATA_FILE_PATH = "./data/french_words.csv"
+ORIGINAL_DATA_FILE_PATH = "./data/french_words.csv"
+LEARN_DATA_FILE_PATH = "./words_to_learn.csv"
 FONT = 'Ariel'
 STUDY_LANGUAGE = "French"
 MOTHER_LANGUAGE = "English"
@@ -14,6 +15,7 @@ all_data = []
 flip_back = None
 language_to_learn = ''
 language_to_translation = ''
+current_word = ""
 # ------------------- Generate a Card information -------------------
 
 
@@ -27,8 +29,6 @@ def check_languages() -> None:
 def selected_word() -> dict:
     global all_data
     word_info = choice(all_data)
-    language_1, language_2 = word_info.keys()
-    word, word_translation = word_info.values()
     return word_info
 
 
@@ -37,6 +37,7 @@ def selected_word() -> dict:
 
 def next_card() -> None:
     global flip_back
+    global current_word
     current_word = selected_word()
     flip_card(image=card_front, title=language_to_learn, word=current_word[language_to_learn], font_color='black')
     flip_back = window.after(
@@ -61,55 +62,63 @@ def flip_card(image: tk.PhotoImage, title: str, word: str, font_color: str) -> N
 
 
 def known_word_flow():
+    global current_word
+    all_data.remove(current_word)
+    export_data = pd.DataFrame(all_data)
+    export_data.to_csv('words_to_learn.csv', index=False)
     next_card()
-    pass
 
 
 def unknown_word_flow():
     next_card()
-    pass
 
 
 if __name__ == "__main__":
-    data = pd.read_csv(DATA_FILE_PATH)
+    try:
+        data = pd.read_csv(LEARN_DATA_FILE_PATH)
+    except FileNotFoundError:
+        original_data = pd.read_csv(ORIGINAL_DATA_FILE_PATH)
+        to_learn = original_data
+    else:
+        to_learn = data
+    finally:
+        all_data = to_learn.to_dict(orient='records')
+        check_languages()
 
-    all_data = data.to_dict(orient='records')
-    check_languages()
+        current_word = selected_word()
 
-    word = selected_word()
+        # ------------------- UI Designer -------------------
+        window = tk.Tk()
+        window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+        window.title("Flash Card")
 
-    # ------------------- UI Designer -------------------
-    window = tk.Tk()
-    window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
-    window.title("Flash Card")
+        # Card Canvas
+        card_front = tk.PhotoImage(file="./images/card_front.png")
+        card_back = tk.PhotoImage(file="./images/card_back.png")
+        canvas = tk.Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+        canvas_image = canvas.create_image(800 // 2, 526 // 2, image=card_front)
+        canvas.grid(row=0, column=0, columnspan=2)
 
-    # Card Canvas
-    card_front = tk.PhotoImage(file="./images/card_front.png")
-    card_back = tk.PhotoImage(file="./images/card_back.png")
-    canvas = tk.Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
-    canvas_image = canvas.create_image(800 // 2, 526 // 2, image=card_front)
-    canvas.grid(row=0, column=0, columnspan=2)
+        # Card Front Language text
+        title_text = canvas.create_text(400, 150, text=language_to_learn, font=(FONT, 40, "italic"))
+        word_text = canvas.create_text(400, 263, text=current_word[language_to_learn], font=(FONT, 60, "bold"))
 
-    # Card Front Language text
-    title_text = canvas.create_text(400, 150, text=language_to_learn, font=(FONT, 40, "italic"))
-    word_text = canvas.create_text(400, 263, text=word[language_to_learn], font=(FONT, 60, "bold"))
+        # Wrong button
+        image_wrong = tk.PhotoImage(file="./images/wrong.png")
+        unknown_button = tk.Button(image=image_wrong, highlightthickness=0, border=0, command=unknown_word_flow)
+        unknown_button.grid(row=1, column=0)
 
-    # Wrong button
-    image_wrong = tk.PhotoImage(file="./images/wrong.png")
-    unknown_button = tk.Button(image=image_wrong, highlightthickness=0, border=0, command=unknown_word_flow)
-    unknown_button.grid(row=1, column=0)
+        # Right button
+        image_right = tk.PhotoImage(file="./images/right.png")
+        know_button = tk.Button(image=image_right, highlightthickness=0, border=0, command=known_word_flow)
+        know_button.grid(row=1, column=1)
 
-    # Right button
-    image_right = tk.PhotoImage(file="./images/right.png")
-    know_button = tk.Button(image=image_right, highlightthickness=0, border=0, command=known_word_flow)
-    know_button.grid(row=1, column=1)
-
-    flip_back = window.after(
-        3000,
-        flip_card,
-        card_back,
-        language_to_translation,
-        word[language_to_translation],
-        'white'
-        )
-    window.mainloop()
+        flip_back = window.after(
+            3000,
+            flip_card,
+            card_back,
+            language_to_translation,
+            current_word[language_to_translation],
+            'white'
+            )
+        window.mainloop()
