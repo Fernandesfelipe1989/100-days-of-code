37a29ac00a7d14c3155b310cf034c1976b57eba1
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-05-10 08:41:23 -0300

    Fixed the email send] bug.

--- birthday-wisher/main.py
+++ birthday-wisher/main.py
@@ -1,10 +1,10 @@
 import datetime as dt
+from email.mime.text import MIMEText
 import smtplib
 from random import choice
 
 from decouple import config
 import pandas as pd
-
 
 PATTERN_MESSAGE = '[NAME]'
 PATH_LETTERS = [
@@ -14,46 +14,53 @@ PATH_LETTERS = [
 ]
 PATH_BIRTHDAY = "birthdays.csv"
 
-birthdays = pd.read_csv('birthdays.csv').to_dict(orient='records')
-current_day = dt.datetime.now()
-letter = ""
-person_birthday = None
 
-host = config("EMAIL_HOST", default='smtp.mailtrap.io')
-port = config("EMAIL_PORT", default=2525)
-user = config("EMAIL_HOST_USER", default="")
-password = config("EMAIL_HOST_PASSWORD", default="")
-sender = config("EMAIl_SENDER", default="test@gmail.com")
-receiver = config("EMAIl_RECEIVER", default="test@gmail.com")
-
-for birthday in birthdays:
-    if birthday['day'] == current_day.day and birthday['month'] == current_day.month:
-        person_birthday = birthday
+def make_message(subject, content):
+    message = MIMEText(content)
+    message["Subject"] = subject
+    message["From"] = sender
+    message["To"] = receiver
+    return message
 
 
-if person_birthday:
-    path_letter = choice(PATH_LETTERS)
-    try:
-        with open(path_letter, 'r') as message:
-            letter = message.read()
-    except FileNotFoundError:
-        letter = f"Happy Birthday {PATTERN_MESSAGE}"
-
-    letter = letter.replace(PATTERN_MESSAGE, person_birthday['name'])
-    message = f"""\
-    Subject: Happy Birthday
-    To: {receiver}
-    From: {sender}
-
-    {letter}"""
-
+def send_email(message):
     with smtplib.SMTP(host=host, port=port) as server:
-        print(server.ehlo())
-        server.starttls()
-        server.ehlo()
         server.login(user=user, password=password)
         server.sendmail(
             from_addr=sender,
             to_addrs=receiver,
-            msg=letter
-            )
+            msg=message.as_string(),
+        )
+
+
+if __name__ == "__main__":
+    birthdays = pd.read_csv('birthdays.csv').to_dict(orient='records')
+    current_day = dt.datetime.now()
+    letter = ""
+    person_birthday = None
+
+    host = config("EMAIL_HOST", default='smtp.mailtrap.io')
+    port = config("EMAIL_PORT", default=2525)
+    user = config("EMAIL_HOST_USER", default="")
+    password = config("EMAIL_HOST_PASSWORD", default="")
+    sender = config("EMAIl_SENDER", default="test@gmail.com")
+    receiver = config("EMAIl_RECEIVER", default="test@gmail.com")
+
+    for birthday in birthdays:
+        if birthday['day'] == current_day.day and birthday['month'] == current_day.month:
+            person_birthday = birthday
+
+    if person_birthday:
+        path_letter = choice(PATH_LETTERS)
+        try:
+            with open(path_letter, 'r') as message:
+                letter = message.read()
+        except FileNotFoundError:
+            letter = f"Happy Birthday {PATTERN_MESSAGE}"
+
+        letter = letter.replace(PATTERN_MESSAGE, person_birthday['name'])
+        message = make_message('Happy Birthday', letter)
+        send_email(message=message)
+
+
+
