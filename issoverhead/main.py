02bbb7f7011e72f80592a21c2924688b5d76e09f
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-05-10 09:29:02 -0300

    WIP

--- issoverhead/main.py
+++ issoverhead/main.py
@@ -4,6 +4,6 @@ URL_BASE = "http://api.open-notify.org/iss-now.json"
 
 if __name__ == "__main__":
     response = requests.get(url=URL_BASE)
-    if response.status_code == 200:
-        data = response.json()
-        print(data)
+    response.raise_for_status()
+    data = response.json()
+    print(data)
