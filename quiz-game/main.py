7243072de2f426833cce4ba057acb48f7608f133
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-05-11 20:09:20 -0300

    Update the code.

--- quiz-game/main.py
+++ quiz-game/main.py
@@ -4,8 +4,4 @@ from ui import QuizInterface
 
 if __name__ == "__main__":
     run_game = True
-    QuizInterface()
-    # while run_game:
-    #     run_game = quiz.still_has_questions()
-
-    print(f"Your final score is {quiz.final_score():.1f}%")
+    QuizInterface(quiz_brain=quiz)
