edb1a0b1e1eb88647e70cb9d96bbfb8cd637a88d
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-05-11 10:23:35 -0300

    Improve the code and create a UI.

--- quiz-game/main.py
+++ quiz-game/main.py
@@ -1,8 +1,11 @@
+
 from quiz import quiz
+from ui import QuizInterface
 
 if __name__ == "__main__":
     run_game = True
-    while run_game:
-        run_game = quiz.still_has_questions()
+    QuizInterface()
+    # while run_game:
+    #     run_game = quiz.still_has_questions()
 
     print(f"Your final score is {quiz.final_score():.1f}%")
