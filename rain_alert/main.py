905a5b5e1670185ba9c961030646e1cf71bdb75d
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-05-13 15:57:43 -0300

    Check if it's rain.

--- rain_alert/main.py
+++ rain_alert/main.py
@@ -10,6 +10,17 @@ if __name__ == "__main__":
         'lat': -23.482050,
         'lon': -47.414291,
         'appid': API_KEY,
+        'exclude': 'minutely,daily'
     }
     response = requests.get(url=BASE_URL, params=parameters)
-    print(response.json())
+    response.raise_for_status()
+    weather_data = response.json()
+    print(weather_data)
+    is_rain = False
+    for hourly_weather in weather_data.get("hourly")[7:20]:
+        weather_info = hourly_weather.get('weather')
+        if weather_info and weather_info[0].get('id') < 700:
+            is_rain = True
+    if is_rain:
+        print("It's going to rain.")
+
