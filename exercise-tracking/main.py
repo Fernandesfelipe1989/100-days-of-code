337e25a5a3f86dcbb0233dbaf1e80c47c7725fb2
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-05-17 09:39:29 -0300

    Create the exercises flow.

--- exercise-tracking/main.py
+++ exercise-tracking/main.py
@@ -1,3 +1,4 @@
+import datetime as dt
 import requests
 
 from decouple import config
@@ -5,9 +6,14 @@ from decouple import config
 API_KEY = config('API_KEY')
 USER_ID = config('USER_ID')
 BASE_URL = 'https://trackapi.nutritionix.com//v2/natural/exercise'
-headers = {
+SHEET_BASE_URL = config('SHEET_BASE_URL')
+
+headers_track = {
     'x-app-id': USER_ID,
     'x-app-key': API_KEY,
+}
+headers_sheet = {
+    'Authorization': f"Bearer {config('SHEET_AUTH_TOKEN')}",
 }
 
 if __name__ == "__main__":
@@ -15,7 +21,21 @@ if __name__ == "__main__":
     exercise_info = {
         'query': exercise,
     }
-    response = requests.post(url=BASE_URL, json=exercise_info, headers=headers)
+    response = requests.post(url=BASE_URL, json=exercise_info, headers=headers_track)
     response.raise_for_status()
-    print(response.json())
+    exercises_data = response.json().get('exercises')
+    today = dt.datetime.today()
 
+    for exercise in exercises_data:
+        exercise_parameters = {
+            'sheet1': {
+                'date': today.strftime("%d/%m/%Y"),
+                'time': today.time().strftime('%H:%M:%S'),
+                'exercise': exercise.get('name').title(),
+                'duration': exercise.get('duration_min'),
+                'calories': exercise.get('nf_calories'),
+            },
+        }
+        response = requests.post(url=SHEET_BASE_URL, json=exercise_parameters, headers=headers_sheet)
+        response.raise_for_status()
+        print(response.text)
