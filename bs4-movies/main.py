8f5631e4023a51055b2687fcbf0ea607d380142b
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-05-26 10:20:00 -0300

    Studying soup selectors.

--- bs4-movies/main.py
+++ bs4-movies/main.py
@@ -5,7 +5,21 @@ with open('./website.html', 'r') as file:
     content = file.read()
 
 soup = BeautifulSoup(content, 'html.parser')
-print(soup.title)
-print(soup.title.string)
-print(soup)
-print(soup.prettify())
+all_anchor_tags = soup.find_all(name='a')
+for tag in all_anchor_tags:
+    print(tag.get('href'))
+
+heading = soup.find(name="h1", id='name')
+print(heading)
+section_heading = soup.find(name='h3', class_="heading")
+print(section_heading.getText())
+print(section_heading.name)
+
+company_url = soup.select_one(selector="p a")
+print(company_url)
+
+name = soup.select_one(selector="#name")
+print(name)
+
+headings = soup.select('.heading')
+print(headings)
