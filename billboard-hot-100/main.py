094853b73bf6590af77c6ab88b4cd32b8b040366
Felipe Fernandes <fernandes.felipe1989@gmail.com>
2022-05-27 09:59:53 -0300

    Create a web scraping to the billboard 100 top music by date.

---
+++ billboard-hot-100/main.py
@@ -0,0 +1,17 @@
+import requests
+from re import sub
+
+from bs4 import BeautifulSoup
+
+SUB_PATTERN = r'[\n\t]'
+BASE_URL = 'https://www.billboard.com/charts/hot-100/'
+
+
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD\n")
+
+url = f'{BASE_URL}{date}/'
+response = requests.get(url=url)
+soup = BeautifulSoup(response.text, 'html.parser')
+song_titles = soup.select("li ul li h3")
+top_100_songs = [sub(pattern=SUB_PATTERN, string=song_title.getText(), repl="") for song_title in song_titles]
+print(top_100_songs)
