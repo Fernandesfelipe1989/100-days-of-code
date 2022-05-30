48399f48e4eb49dbee0ec2c779fe35a098603812
Felipe Fernandes <felipefernandes@pop-os.localdomain>
2022-05-30 09:48:06 -0300

    Studying Selenium find_elements

--- selenium_day_48/main.py
+++ selenium_day_48/main.py
@@ -1,3 +1,4 @@
+import datetime as dt
 from decouple import config
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -8,8 +9,15 @@ options.add_argument("--disable-dev-shm-usage")
 chrome_driver_path = config('CHROME_DRIVER_PATH')
 driver_service = Service(executable_path=chrome_driver_path)
 driver = webdriver.Chrome(service=driver_service, options=options)
+driver.get("https://www.python.org/")
+events_names = driver.find_elements(by="xpath", value="//div/section/div[3]/div[@class='medium-widget event-widget "
+                                                      "last']/div/ul[@class='menu']/li/a")
 
-driver.get("https://www.amazon.com.br/Geladeira-Brastemp-Frost-Inverse-litros/dp/B079ZHBWKK/")
-price = driver.find_element(by="class name", value='a-price-whole')
-print(price.text)
+events_dates = driver.find_elements(by="css selector", value='div.last div ul li time')
+
+
+events_info = {index: {'time': events_dates[index].get_attribute('datetime').split("T")[0], 'name': event_name.text}
+               for index, event_name in enumerate(events_names)}
+
+print(events_info)
 driver.close()
