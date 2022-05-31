65ac35793cf5b5d5e3cfa4d8c25695a89c47d0cf
Felipe Fernandes <felipefernandes@pop-os.localdomain>
2022-05-31 09:26:24 -0300

    Studying interactions.

--- selenium_day_48/interaction.py
+++ selenium_day_48/interaction.py
@@ -11,18 +11,16 @@ options.add_argument("--disable-dev-shm-usage")
 chrome_driver_path = config('CHROME_DRIVER_PATH')
 driver_service = Service(executable_path=chrome_driver_path)
 driver = webdriver.Chrome(service=driver_service, options=options)
-driver.get("https://en.wikipedia.org/wiki/Main_Page")
+driver.get("https://secure-retreat-92358.herokuapp.com/")
 
-article_count = driver.find_element(by="css selector", value="#articlecount a")
-# article_count.click()
-
-wikidata = driver.find_element(by="link text", value='Wikidata')
-# wikidata.click()
-
-search = driver.find_element(by=By.NAME, value='search')
-search.send_keys("Python")
-search.send_keys(Keys.ENTER)
-
+first_name = driver.find_element(by=By.NAME, value="fName")
+first_name.send_keys("Teste")
+last_name = driver.find_element(by=By.NAME, value='lName')
+last_name.send_keys("Da Silva")
+email = driver.find_element(by=By.NAME, value='email')
+email.send_keys("test@gmail.com")
+sing_up = driver.find_element(by=By.CSS_SELECTOR, value="button")
+sing_up.click()
 
 driver.quit()
 
