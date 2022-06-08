65e81448d970b32535ebf8d63156e4318118659c
Felipe Fernandes <felipefernandes@pop-os.localdomain>
2022-06-08 14:30:00 -0300

    Created the post form flow.

--- zillow_rental_research/main.py
+++ zillow_rental_research/main.py
@@ -1,8 +1,12 @@
 import requests
 import re
+from time import sleep
 
 from bs4 import BeautifulSoup
 from decouple import config
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
 
 class ZillowRentalResearch:
@@ -18,9 +22,19 @@ class ZillowRentalResearch:
         self.listings = []
         self.prices = []
         self.address = []
+
+        # Clean string config
         self.ZILLOW_URL_PATTERN = 'https://www.zillow.com'
         self.PRICE_PATTERN = r'([0-9].[0-9]+)'
         self.URL_PATTERN = r'(http..//)'
+
+        # Selenium Config
+        options = webdriver.ChromeOptions()
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        chrome_driver_path = config('CHROME_DRIVER_PATH')
+        driver_service = Service(executable_path=chrome_driver_path)
+        self.driver = webdriver.Chrome(service=driver_service, options=options)
 
     def clean_price(self, price):
         price_cleaned = re.findall(self.PRICE_PATTERN, price)
@@ -47,7 +61,35 @@ class ZillowRentalResearch:
             listings = soup.find_all(name='a', class_="list-card-link", href=True)
             self.listings = [self.clean_url(listing['href']) for listing in listings]
 
+    def post_renting_form(self):
+        self.driver.get(url=config('FORM_URL'))
+        for rent_index in range(0, len(self.address)):
+            try:
+                address_tag = self.driver.find_element(by=By.XPATH, value='/html/body/div/div[2]/form/div[2]/div/div['
+                                                                          '2]/div[1]/div/div/div[2]/div/div['
+                                                                          '1]/div/div[1]/input')
+                address_tag.send_keys(self.address[rent_index])
+                price_tag = self.driver.find_element(by=By.XPATH, value='/html/body/div/div[2]/form/div[2]/div/div['
+                                                                        '2]/div[2]/div/div/div[2]/div/div[1]/div/div['
+                                                                        '1]/input')
+                price_tag.send_keys(self.prices[rent_index])
+                link_tag = self.driver.find_element(by=By.XPATH, value='/html/body/div/div[2]/form/div[2]/div/div['
+                                                                       '2]/div[3]/div/div/div[2]/div/div[1]/div/div['
+                                                                       '1]/input')
+                link_tag.send_keys(self.listings[rent_index])
+
+                button_tag = self.driver.find_element(by=By.XPATH, value='/html/body/div/div[2]/form/div[2]/div/div['
+                                                                         '3]/div[1]/div[1]/div')
+                button_tag.click()
+                sleep(2)
+                return_tag = self.driver.find_element(by=By.XPATH, value='/html/body/div[1]/div[2]/div[1]/div/div[4]/a')
+                return_tag.click()
+                sleep(2)
+            except Exception as error:
+                print(error)
+
 
 if __name__ == "__main__":
     rental_research = ZillowRentalResearch()
     rental_research.get_renting_info()
+    rental_research.post_renting_form()
