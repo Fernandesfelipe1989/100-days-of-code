21eb5081301fad9143fedb534b1d6ec0ea7f0f06
Felipe Fernandes <felipefernandes@pop-os.localdomain>
2022-06-14 09:37:04 -0300

    Studying Flask routes.

--- hello-flask/main.py
+++ hello-flask/main.py
@@ -3,15 +3,15 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!<p>"
+@app.route("/<name>")
+def hello_world(name):
+    return f"<p>Hello, {name}!<p>"
 
 
 @app.route("/bye")
 def say_bye():
-    return "<h1>Bye</h1>"
+    return "<h1 style='text-align: center;'>Bye</h1>"
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
