4414a63cb757a691bd90d9f4ccf4a88ac737afc7
Felipe Fernandes <felipefernandes@pop-os.localdomain>
2022-06-14 09:53:40 -0300

    Studying decorator.

--- hello-flask/main.py
+++ hello-flask/main.py
@@ -3,9 +3,33 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/<name>")
-def hello_world(name):
-    return f"<p>Hello, {name}!<p>"
+def make_bold(func):
+    def make_bold_wrapper():
+        message = func()
+        return '<b>' + message + '</b>'
+    return make_bold_wrapper
+
+
+def make_emphasis(func):
+    def make_emphasis_wrapper():
+        message = func()
+        return '<em>' + message + '</em>'
+    return make_emphasis_wrapper
+
+
+def make_underlined(func):
+    def make_underlined_wrapper():
+        message = func()
+        return '<u>' + message + '</u>'
+    return make_underlined_wrapper
+
+
+@app.route("/")
+@make_bold
+@make_emphasis
+@make_underlined
+def hello_world():
+    return f"Hello!"
 
 
 @app.route("/bye")
