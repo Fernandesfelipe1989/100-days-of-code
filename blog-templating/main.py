457976b2d2f503f7fe091e60b0c230cf2ac8b6ae
Felipe Fernandes <felipefernandes@pop-os.localdomain>
2022-06-16 18:33:21 -0300

    Create the post flow.

--- blog-templating/main.py
+++ blog-templating/main.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template
 
+from post import Post
 
 app = Flask(__name__)
+post = Post()
+
+
+@app.route("/post/<int:id>")
+def get_post(id):
+    context = dict(city='Sorocaba', post=post.get_post(id=id))
+    return render_template('post.html', **context)
+
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    post_data = post.get_posts()
+    context = dict(city='Sorocaba', all_post=post_data)
+    return render_template("index.html", **context)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- blog-templating/post.py
+++ blog-templating/post.py
@@ -1,2 +1,17 @@
+import requests
+
+
 class Post:
-    pass
+    BASE_URL = "https://api.npoint.io/790a028aa38e1c2bcd50"
+
+    def __init__(self):
+        response = requests.get(url=self.BASE_URL)
+        response.raise_for_status()
+        self.post = response and response.json()
+
+    def get_posts(self):
+        return self.post
+
+    def get_post(self, id):
+        index = id - 1
+        return self.post[index] if len(self.post) > index >= 0 else {}
