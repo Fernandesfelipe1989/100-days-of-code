088b358f4b4693471c50b2b0cebcdc7e13db2965
Felipe Fernandes <felipefernandes@pop-os.localdomain>
2022-06-16 17:37:16 -0300

    Create a guess name flow.

--- day-57/server.py
+++ day-57/server.py
@@ -1,8 +1,29 @@
 from datetime import datetime as dt
+import requests
 
 from flask import Flask, render_template
 
+
+BASE_URL_AGIFY = "https://api.agify.io?"
+BASE_URL_GENDERRIZE = "https://api.genderize.io"
+
 app = Flask(__name__)
+
+
+@app.route("/guess/<name>")
+def guess(name):
+    context = {}
+    parameters = {
+        'name': name,
+    }
+    response_gender = requests.get(url=BASE_URL_GENDERRIZE, params=parameters)
+    response_age = requests.get(url=BASE_URL_AGIFY, params=parameters)
+    if response_age.status_code == 200 and response_gender.status_code == 200:
+        data_gender = response_gender and response_gender.json()
+        data_age = response_age and response_age.json()
+        print(data_age)
+        context = dict(data_gender, **data_age)
+    return render_template('guess.html', **context)
 
 
 @app.route("/")
