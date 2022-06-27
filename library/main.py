49d581a910d004896fdef47a512438bbaa0ba1a8
Felipe Fernandes <felipefernandes@pop-os.localdomain>
2022-06-27 16:43:20 -0300

    Create a database flow.

--- library/main.py
+++ library/main.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
 
+from flask import Flask, render_template, request, redirect, url_for
+db_id = 1
 app = Flask(__name__)
 
+
+def connect_db(command: str):
+    db = sqlite3.connect("books-collection.db")
+    print(command)
+    cursor = db.cursor()
+    cursor.execute(
+       command
+    )
+    db.commit()
+
+
+INSERT = "INSERT INTO books VALUES({}, '{}', '{}', '{}')"
 all_books = []
 
 
@@ -10,18 +24,22 @@ def home():
     return render_template('index.html', books=all_books)
 
 
-@app.route("/add",  methods=["GET", "POST"])
+@app.route("/add", methods=["GET", "POST"])
 def add():
+    global db_id
     if request.method == "POST":
         name = request.form.get('name')
         author = request.form.get('author')
         rating = request.form.get('rating')
         if name and author:
             book = dict(
+                id=db_id,
                 title=name,
                 author=author,
                 rating=rating,
             )
+            connect_db(INSERT.format(db_id, name, author, rating))
+            db_id += 1
             all_books.append(book)
         return redirect(url_for("home"))
     return render_template("add.html")
@@ -29,4 +47,3 @@ def add():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
