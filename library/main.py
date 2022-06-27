76aeb568f4b5eb837f724281218ec4ad23d85613
Felipe Fernandes <felipefernandes@pop-os.localdomain>
2022-06-27 17:28:21 -0300

    Create the add flow.

--- library/main.py
+++ library/main.py
@@ -1,49 +1,58 @@
-import sqlite3
-
 from flask import Flask, render_template, request, redirect, url_for
-db_id = 1
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 
-
-def connect_db(command: str):
-    db = sqlite3.connect("books-collection.db")
-    print(command)
-    cursor = db.cursor()
-    cursor.execute(
-       command
-    )
-    db.commit()
+db = SQLAlchemy(app)
 
 
-INSERT = "INSERT INTO books VALUES({}, '{}', '{}', '{}')"
-all_books = []
+class Book(db.Model):
+    __tablename__ = 'books'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), unique=True, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return self.title
+
+
+db.create_all()
 
 
 @app.route('/')
 def home():
+    all_books = db.session.query(Book).all()
     return render_template('index.html', books=all_books)
+
+
+@app.route('/edit/<int:id>', methods=["GET", "POST"])
+def edit(id):
+    book = Book.query.filter_by(id=id)
+
+    pass
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
-    global db_id
     if request.method == "POST":
         name = request.form.get('name')
         author = request.form.get('author')
         rating = request.form.get('rating')
         if name and author:
-            book = dict(
-                id=db_id,
+            data = dict(
                 title=name,
                 author=author,
                 rating=rating,
             )
-            connect_db(INSERT.format(db_id, name, author, rating))
-            db_id += 1
-            all_books.append(book)
+            book = Book(**data)
+            db.session.add(book)
         return redirect(url_for("home"))
     return render_template("add.html")
 
 
 if __name__ == "__main__":
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///new-books-collection.db"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     app.run(debug=True)
