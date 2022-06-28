149c97ea9aa6d0c6a97abc8c469184f2ab588921
Felipe Fernandes <felipefernandes@pop-os.localdomain>
2022-06-28 19:16:53 -0300

    Create the add flow.

--- movie_project/forms.py
+++ movie_project/forms.py
@@ -1,6 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import FloatField, StringField, SubmitField
+from wtforms import FloatField, IntegerField, StringField, SubmitField
 from wtforms.validators import DataRequired
+
+
+class MovieAddForm(FlaskForm):
+    title = StringField(label="Movie's Title")
+    year = IntegerField(label="Movie's Year")
+    description = StringField(label="Description")
+    rating = FloatField(label="Rating")
+    ranking = IntegerField(label="Movie's ranking")
+    review = StringField(label="Review")
+    img_url = StringField(label="Movie's image url")
+    submit = SubmitField(label="Save")
 
 
 class MovieForm(FlaskForm):
--- movie_project/views.py
+++ movie_project/views.py
@@ -1,11 +1,8 @@
-
 from flask import Blueprint
-
 
 from flask import render_template, redirect, url_for, request
 
-
-from forms import MovieForm
+from forms import MovieAddForm, MovieForm
 from models import db, MovieModel
 
 bp = Blueprint('views', __name__, url_prefix='/')
@@ -15,6 +12,33 @@ bp = Blueprint('views', __name__, url_prefix='/')
 def home():
     all_movies = MovieModel.query.all()
     return render_template("index.html", movies=all_movies)
+
+
+@bp.route("/add/", methods=["GET", "POST"])
+def add():
+    form = MovieAddForm()
+    if form.is_submitted():
+        movie = MovieModel(
+            title=form.data.get("title"),
+            year=form.data.get("year"),
+            description=form.data.get("description"),
+            rating=form.data.get("rating"),
+            ranking=form.data.get("ranking"),
+            review=form.data.get("review"),
+            img_url=form.data.get("img_url"),
+        )
+        db.session.add(movie)
+        db.session.commit()
+        return redirect(url_for("views.home"))
+    return render_template('add.html', form=form)
+
+
+@bp.route("delete/<int:id>", methods=['GET', 'POST'])
+def delete(id):
+    movie = MovieModel.query.filter_by(id=id).first()
+    db.session.delete(movie)
+    db.session.commit()
+    return redirect(url_for("views.home"))
 
 
 @bp.route("/edit/<int:id>", methods=["GET", "POST"])
@@ -29,12 +53,3 @@ def edit(id):
         return redirect(url_for("views.edit", id=id))
 
     return render_template('edit.html', form=form, movie=movie)
-
-
-@bp.route("delete/<int:id>", methods=['GET', 'POST'])
-def delete(id):
-    movie = MovieModel.query.filter_by(id=id).first()
-    db.session.delete(movie)
-    db.session.commit()
-    return redirect(url_for("views.home"))
-
