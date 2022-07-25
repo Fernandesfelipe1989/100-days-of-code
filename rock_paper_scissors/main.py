ceb147bb433e406c0901dda587b33412f8f7c6bc
Felipe Fernandes <felipefernandes@pop-os.localdomain>
2022-07-25 12:55:23 -0300

    Update the game flow.

--- rock_paper_scissors/main.py
+++ rock_paper_scissors/main.py
@@ -20,7 +20,8 @@ class RockPaperScissorSimulator:
     }
 
     def __init__(self):
-        self.computer_option = choice(self.OPTIONS)
+        self.user_choice = None
+        self.computer_choice = None
 
     def get_user_choice(self):
         user_option = input(f"What do you choose? Type {self.show_the_game_options()}\n")
@@ -31,10 +32,11 @@ class RockPaperScissorSimulator:
 
     def result_game(self) -> str:
         first_option = f"User choose: \n{images_options.get(self.user_choice, '')}\n"
-        second_option = f"Computer choose: \n{images_options.get(self.computer_option , '')}\n"
+        second_option = f"Computer choose: \n{images_options.get(self.computer_choice, '')}\n"
         return first_option + second_option
 
     def choose_winner(self) -> str:
+        self.computer_choice = choice(self.OPTIONS)
         condition = conditions.get(self.user_choice, None)
         return self.conditions_to_win(condition)
 
@@ -42,9 +44,9 @@ class RockPaperScissorSimulator:
         result = "Draw"
         if not condition:
             return "Invalid option"
-        if condition['win'] == self.computer_option:
+        if condition['win'] == self.computer_choice:
             result = "You Win"
-        if condition['lose'] == self.computer_option:
+        if condition['lose'] == self.computer_choice:
             result = "You lose"
         return result
 
